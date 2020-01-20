c0aae70f049c9379611194bfa9ed415dc8e25e86
Cody Konior <cody@codykonior.com>
2020-01-20 13:26:43 +0800

    Update DataCollection.ps1

--- DataCollection.ps1
+++ DataCollection.ps1
@@ -1,12 +1,19 @@
-while ($true) {
-    $date = (Get-Date).ToString("o")
-    $data = &python3 modem-technicolour-httpstats.py
-    $data = $data | ForEach-Object { $_.Trim() } | Where-Object { $_ }
-    $hashtable = [ordered] @{ Date = $date }
-    for ($i = 0; $i -lt $data.Count; $i += 2) {
-        $hashtable.($data[$i]) = $data[$i + 1]
-    }
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
 
-    [PSCustomObject] $hashtable | Export-Csv -Path Data.csv -Append
+while ($true) {
+    try {
+        $date = (Get-Date).ToString("o")
+        $data = &python3 modem-technicolour-httpstats.py
+        $data = $data | ForEach-Object { $_.Trim() } | Where-Object { $_ }
+        $hashtable = [ordered] @{ Date = $date }
+        for ($i = 0; $i -lt $data.Count; $i += 2) {
+            $hashtable.($data[$i]) = $data[$i + 1]
+        }
+
+        [PSCustomObject] $hashtable | Export-Csv -Path Data.csv -Append
+    } catch {
+        "Caught exception and continuing: $_"
+    }
     Start-Sleep -Seconds 30
 }
